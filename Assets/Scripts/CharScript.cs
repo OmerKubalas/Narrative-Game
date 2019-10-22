2b9d4789cda41c1e3ffeffbbb81cfb1b0f6399ca
Omer Kubalas <omer.kubalas@bilgiedu.net>
2019-10-22 19:59:13 +0300

    Stuff

---
+++ Assets/Scripts/CharScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharScript : MonoBehaviour
+{
+    public Rigidbody2D playerbody;
+    public float speed;
+    public float jumpForce;
+    float jumps;
+    int PlayerState;
+
+    GameObject NPC;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumps = 1;
+        speed = 10;
+        jumpForce = 16.5f;
+        PlayerState = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerState != 2)
+        {
+            playerbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), playerbody.velocity.y / speed) * speed;
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && jumps > 0)
+            {
+                jumps = 0;
+                playerbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.localScale = new Vector3(1, 3, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.localScale = new Vector3(-1, 3, 1);
+            }
+        }
+
+        if (PlayerState == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) && NPC.GetComponent<NPCScript>().alive)
+            {
+                //talk here
+                PlayerState = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && NPC.GetComponent<NPCScript>().alive)
+            {
+                //absorb life here
+                PlayerState = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                //give life here
+                PlayerState = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                //cancel
+                PlayerState = 0;
+            }
+        }
+
+        if (PlayerState == 1 && Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerState = 2;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Ground")
+        {
+            jumps = 1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "NPC")
+        {
+            PlayerState = 1;
+            NPC = col.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "NPC")
+        {
+            PlayerState = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/NPCScript.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+    public bool alive; //took life, give life
+    public bool sick;
+    bool InRange;
+    int NPCState;
+    public string[] dialogue;
+    public GameObject spacePopup, OptionsPopup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NPCState = 0;
+        if (sick)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(120, 215, 70);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+        }
+
+        if (alive)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            transform.position = new Vector3(transform.position.x, transform.position.y-1, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && InRange)
+        {
+            spacePopup.transform.position = new Vector2(999, 999);
+            if (alive)
+            {
+                OptionsPopup.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+                OptionsPopup.transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+            }
+            else
+            {
+                OptionsPopup.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
+                OptionsPopup.transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
+            }
+            OptionsPopup.transform.position = new Vector2(transform.position.x, transform.position.y + 4);
+            NPCState = 1;
+        }
+        if (NPCState == 1 && alive && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //talk
+            OptionsPopup.transform.position = new Vector2(999, 999);
+            NPCState = 0;
+        }
+        if (NPCState == 1 && alive && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            alive = false;
+            //absorb
+            OptionsPopup.transform.position = new Vector2(999, 999);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1, 0);
+            NPCState = 0;
+        }
+        if (NPCState == 1 && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            alive = true;
+            //grant
+            OptionsPopup.transform.position = new Vector2(999, 999);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            transform.position = new Vector3(transform.position.x, transform.position.y+1, 0);
+            NPCState = 0;
+        }
+        if (NPCState == 1 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //cancel
+            OptionsPopup.transform.position = new Vector2(999, 999);
+            NPCState = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            spacePopup.transform.position = new Vector2(transform.position.x, transform.position.y + 2);
+            InRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            spacePopup.transform.position = new Vector2(999, 999);
+            InRange = false;
+        }
+    }
+}
