3525990c04aeae339690cfa2276caf1397577811
Gina Al Halabi <ghina7alabi@gmail.com>
2019-11-06 21:39:57 +0300

    Made NPC Narration more customizable, and added Mother-Son Scenario
    Also added narration to all the other NPCs (4)

--- Assets/Scripts/NPCScript.cs
+++ Assets/Scripts/NPCScript.cs
@@ -11,10 +11,14 @@ public class NPCScript : MonoBehaviour
 
     //NarrationSpeeches
     public Narration regularSpeech;
-    public Narration specialConditionSpeech;
     public Narration aliveGiveLifeSpeech;
     public Narration aliveTakeLifeSpeech;
     public Narration deadGiveLifeSpeech;
+
+    //NarrationConditions
+    public string npcSituation;
+    public GameObject conditionedByNPC; //the npc which conditions this npc (ex: mother conditions son)
+
 
     // Start is called before the first frame update
     void Start()
@@ -78,6 +82,41 @@ public class NPCScript : MonoBehaviour
             CharScript.PlayerState = 3;
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         }
+
+
+        //NarrationCases (we may customize any npc speech to our liking here)
+        switch (npcSituation)
+        {
+            case "Mother":
+                if (conditionedByNPC.GetComponent<NPCScript>().alive) //if son is alive
+                {
+                    regularSpeech.phrases[0].text = "Please, help my son! Food is nowhere to be found, he is very sick! I offer my life if you must!";
+                    deadGiveLifeSpeech.phrases[0].text = "You've reunited me with my son once more.. Thank you.";
+                }
+                else //if he is dead
+                {
+                    regularSpeech.phrases[0].text = "My son! What have you done?!";
+                    deadGiveLifeSpeech.phrases[0].text = "What is the point of life when it has rid you of all you love...";
+                }
+                break;
+
+            case "Son":
+                if (conditionedByNPC.GetComponent<NPCScript>().alive) //if mother is alive
+                {
+                    regularSpeech.phrases[0].text = "I am very sick... My mom keeps saying it's because I got a flu... but they're usually not this painful.";
+                    deadGiveLifeSpeech.phrases[0].text = "Mother.. I missed her very much. The sickness is still roaming around.";
+                }
+                else //if mother is dead
+                {
+                    regularSpeech.phrases[0].text = "Oh no, mom! I have nobody left!";
+                    deadGiveLifeSpeech.phrases[0].text = "You've brought me back to life.. It's meaningless without my mom.";
+                }
+                break;
+
+            default:
+                break;
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D col)
