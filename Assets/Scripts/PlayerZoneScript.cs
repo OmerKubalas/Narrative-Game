656fd5cd39321b7d22a3f0200b28c35c3e801f54
Omer Kubalas <omer.kubalas@bilgiedu.net>
2019-12-31 04:54:36 +0300

    a

--- Assets/Scripts/PlayerZoneScript.cs
+++ Assets/Scripts/PlayerZoneScript.cs
@@ -6,12 +6,6 @@ public class PlayerZoneScript : MonoBehaviour
 {
     public GameObject Player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
